9a8b59ac264ef65a481d7e293fa2c26bb22c0c11
islam <ihamdy@trudoc24x7.com>
2020-10-02 13:59:15 +0200

    Add tests with filters, update readme.md

--- tests/Feature/ListUsersTest.php
+++ tests/Feature/ListUsersTest.php
@@ -4,6 +4,9 @@
 namespace Tests\Feature;
 
 
+use App\ParentAp\DataProviders\AllProviders;
+use App\ParentAp\DataProviders\ProviderX;
+use App\ParentAp\Services\UsersService;
 use Tests\TestCase;
 
 class ListUsersTest extends TestCase
@@ -45,6 +48,28 @@ class ListUsersTest extends TestCase
                 'errors' => ["The selected provider is invalid."],
             ]
         );
+    }
+
+    /** @test */
+    public function list_users_with_filters()
+    {
+        $response = $this->get('api/v1/users?provider=DataProviderX&status=authorised&balanceMin=200');
+        $response->assertStatus(200);
+        $responseData = json_decode($response->getContent(),true);
+        $userService = new UsersService(new ProviderX());
+        $filteredData = $userService->listData(['status'=>'authorised','balanceMin'=>200]);
+        $this->assertEquals(count($responseData['data']),count($filteredData));
+    }
+
+    /** @test */
+    public function list_users_from_all_providers_with_filters()
+    {
+        $response = $this->get('api/v1/users?status=authorised&balanceMin=200');
+        $response->assertStatus(200);
+        $responseData = json_decode($response->getContent(),true);
+        $userService = new UsersService(new AllProviders());
+        $filteredData = $userService->listData(['status'=>'authorised','balanceMin'=>200]);
+        $this->assertEquals(count($responseData['data']),count($filteredData));
     }
 
 }
